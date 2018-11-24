774e9cbff956118ab2a2ac5f29d462e3337b1df6
T1duS <udit121sanghi@gmail.com>
2018-11-24 13:05:26 +0530

    Used cosine similarity

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 import string
 from nltk.corpus import stopwords
 from collections import Counter
 import os
 import sys
-from math import log10
+from math import log10, sqrt
 
 stop_words = stopwords.words('english')
 stop_words.sort()
@@ -49,6 +49,19 @@ def preprocess(restart=False):
 			corpus_freq = eval(data_file.read())
 	total = sum(corpus_freq.values())		
 
+def cos_similarity(d1, d2):
+	dotprod = mod_d1 = mod_d2 = 0
+	for i in d1:
+		if i in d2:
+			dotprod += d1[i]*d2[i]
+	for i in d1:
+		mod_d1 += d1[i]*d1[i]
+	for i in d2:
+		mod_d2 += d2[i]*d2[i]
+	mod_d1 = sqrt(mod_d1)
+	mod_d2 = sqrt(mod_d2)	
+	return dotprod/(mod_d1*mod_d2)
+
 if __name__ == '__main__':
 	
 
@@ -62,16 +75,38 @@ if __name__ == '__main__':
 		exit()
 
 	inpy = open(sys.argv[1], "r")
-	doc_freq = clean_text(inpy.read())
+	file_text = inpy.read()
+	doc_freq = clean_text(file_text)
+	sentences = sent_tokenize(file_text)
 	inpy.close()
 
-	tfidf = []
+	tfidf = {}
 
 	for i in doc_freq:
-		tfidf.append([doc_freq[i]*log10(total/(corpus_freq.get(i,0)+1)), i])
+		tfidf[i] = doc_freq[i]*log10(total/(corpus_freq.get(i,0)+1))
 
-	tfidf.sort(reverse=True)
+	tfidfsent = []
 
-	print("Possible topics are ---")
-	for i in range(min(5, len(tfidf))):
-		print(tfidf[i][1])
+	for sent in sentences:
+		cleansent = clean_text(sent)
+		vec = {}
+		for i in cleansent:
+			vec[i] = tfidf[i] 
+		tfidfsent.append(vec)
+	
+	n = len(tfidfsent)
+	score = [0]*(n+1)		
+	for i in range(n):
+		for j in range(i+1,n):
+			similar = cos_similarity(tfidfsent[i],tfidfsent[j])
+			score[i] += similar
+			score[j] += similar
+
+	mx = score[0]
+	sent = sentences[0]
+	for i in range(1,n):
+		if score[i] > mx:
+			mx = score[i]
+			sent = sentences[i]
+
+	print(sent)					
